f5fb7e658ff2755ff38ff796ca0ab0e1700bc372
mihai209 <sbarceamihai222@gmail.com>
2025-12-31 23:04:48 +0200

    feat

--- os/kernel/cmds/shutdown.cpp
+++ os/kernel/cmds/shutdown.cpp
@@ -1,10 +1,32 @@
 #include "command.h"
 
+static inline void outb(unsigned short port, unsigned char val) {
+    asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
 static inline void outw(unsigned short port, unsigned short val) {
     asm volatile ("outw %0, %1" : : "a"(val), "Nd"(port));
 }
 
 void cmd_shutdown(const char*) {
-    outw(0x604, 0x2000); // QEMU / Bochs poweroff
-    while (1);
+
+    // QEMU (new)
+    outw(0x604, 0x2000);
+
+    // QEMU (old)
+    outw(0xB004, 0x2000);
+
+    // Bochs
+    outw(0x4004, 0x3400);
+
+    // VirtualBox
+    outw(0x4004, 0x3400);
+
+    // ACPI / APM fallback
+    outb(0xB2, 0x20);
+
+    // Dacă nimic nu merge → halt
+    for (;;) {
+        asm volatile("cli; hlt");
+    }
 }
