188b510ea2781ad596ec2ab32fcc015848f9f80d
mihai209 <sbarceamihai222@gmail.com>
2026-01-01 22:06:03 +0200

    feat

--- os/kernel/boot.asm
+++ os/kernel/boot.asm
@@ -1,4 +1,3 @@
-; kernel/boot.asm
 BITS 32
 
 section .multiboot
@@ -7,28 +6,12 @@ align 4
     dd 0x00000000
     dd -(0x1BADB002)
 
-section .bss
-align 16
-stack_bottom:
-    resb 16384          ; 16 KB stack
-stack_top:
-
 section .text
 global _start
 extern kernel_main
 
 _start:
     cli
-
-    ; setup stack
-    mov esp, stack_top
-
-    ; GRUB passes:
-    ; eax = magic
-    ; ebx = multiboot info ptr
-
-    push ebx             ; arg2
-    push eax             ; arg1
     call kernel_main
 
 .hang:
