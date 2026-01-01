a4f60c89c937228827593e73a916a038e04f7718
mihai209 <sbarceamihai222@gmail.com>
2026-01-01 21:59:34 +0200

    feat

--- os/kernel/boot.asm
+++ os/kernel/boot.asm
@@ -1,31 +1,35 @@
 ; kernel/boot.asm
-; Multiboot header + entry that forwards (magic, addr) to kernel_main
-; NASM syntax
+BITS 32
 
 section .multiboot
 align 4
-    dd 0x1BADB002        ; magic number
-    dd 0x00              ; flags (0 = minimal; you can set bits if you want)
-    dd -(0x1BADB002)     ; checksum
+    dd 0x1BADB002
+    dd 0x00000000
+    dd -(0x1BADB002)
+
+section .bss
+align 16
+stack_bottom:
+    resb 16384          ; 16 KB stack
+stack_top:
 
 section .text
 global _start
 extern kernel_main
 
 _start:
-    cli                 ; disable interrupts (safe start)
+    cli
 
-    ; GRUB (multiboot) sets:
-    ;   EAX = magic
-    ;   EBX = pointer to multiboot_info struct
-    ;
-    ; We must pass them to C kernel_main as cdecl arguments:
-    ; push args right-to-left: first push EBX (addr), then EAX (magic),
-    ; then call kernel_main which is declared as: void kernel_main(uint32_t magic, uint32_t addr);
+    ; setup stack
+    mov esp, stack_top
 
-    push    ebx         ; push multiboot_info pointer (addr)
-    push    eax         ; push magic
-    call    kernel_main
+    ; GRUB passes:
+    ; eax = magic
+    ; ebx = multiboot info ptr
+
+    push ebx             ; arg2
+    push eax             ; arg1
+    call kernel_main
 
 .hang:
     hlt
